88523bd1550381cc290289fa6fec1c3c04d9260d
Michael Odaba <odabamichael1@gmail.com>
2026-03-27 01:03:31 +0100

    update summaryAPI

--- client/src/services/SummaryAPI.ts
+++ client/src/services/SummaryAPI.ts
@@ -2,19 +2,19 @@ export const baseURL: string =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:8009/";
 const summaryApi = {
   register: {
-    url: "api/user/register",
+    url: "/api/user/register",
     method: "post",
   },
   login: {
-    url: "api/user/login",
+    url: "/api/user/login",
     method: "post",
   },
   logout: {
-    url: "api/user/logout",
+    url: "/api/user/logout",
     method: "get",
   },
   getUser: {
-    url: "api/user/get-user",
+    url: "/api/user/get-user",
     method: "get",
   },
 };
