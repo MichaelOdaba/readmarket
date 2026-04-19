5ffa6c649624bde4dc882cb667e53586934ea6e6
Michael Odaba <odabamichael1@gmail.com>
2026-04-19 22:00:38 +0100

    fixed auth bug

--- client/src/utils/customAxios.ts
+++ client/src/utils/customAxios.ts
@@ -2,16 +2,7 @@ import axios from "axios";
 
 const customAxios = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8009/",
-  withCredentials: true,
-});
-
-// attach token to every request
-customAxios.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  withCredentials: true, // This automatically sends HTTP-only cookies
 });
 
 export default customAxios;
